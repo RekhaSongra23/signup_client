4fc0acd9770099d68e33e23046252b7e899bd858
RekhaSongra23 <152592009+RekhaSongra23@users.noreply.github.com>
2024-01-30 17:01:59 +0530

    hi

---
+++ src/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, email, password);
+    axios
+      .post("http://localhost:8000/Login", {
+        username: username,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res);
+        alert("data submitted");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        setUsername=""
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        setPassword=""
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+export default Login;
